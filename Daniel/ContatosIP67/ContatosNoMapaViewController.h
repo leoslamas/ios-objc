909ddefb80296b946f80c1f85da2f188a1b08360
Leo Lamas <leoslamas@gmail.com>
2022-08-20 04:58:58 -0300

    Delete Daniel directory

--- Daniel/ContatosIP67/ContatosNoMapaViewController.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-#import <MapKit/MKMapView.h>
-
-@interface ContatosNoMapaViewController : UIViewController <MKMapViewDelegate>
-
-@property(weak, nonatomic) IBOutlet MKMapView *mapa;
-@property(weak, nonatomic) NSMutableArray *contatos;
-
-@end
